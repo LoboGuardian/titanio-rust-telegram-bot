65d4e3e10b3d7526c64a7514352549267ddf8610
aodihis <aodihis@gmail.com>
2025-04-14 21:57:29 +0700

    Issue#12 - Fix error when city not found in /weather (#33)

--- src/commands/weather.rs
+++ src/commands/weather.rs
@@ -1,4 +1,5 @@
-use reqwest::Client;
+use log::info;
+use reqwest::{Client, StatusCode};
 use serde::Deserialize;
 use teloxide::{prelude::*, types::Message};
 
@@ -22,24 +23,36 @@ struct WeatherDesc {
 }
 
 pub async fn handle_weather(bot: Bot, msg: Message, city: String) -> ResponseResult<()> {
+    if city.is_empty() {
+        let reply = "⚠️ Please enter a valid city name.".to_string();
+        bot.send_message(msg.chat.id, reply).await?;
+        return Ok(());
+    }
+
     let url = format!("https://wttr.in/{}?format=j1", city);
 
     let client = Client::new();
     let response = client.get(&url).send().await;
-
     let reply = match response {
-        Ok(resp) => match resp.json::<WeatherData>().await {
-            Ok(data) => {
-                let temp = &data.current_condition[0].temp_c;
-                let desc = data.current_condition[0]
-                    .weather_desc
-                    .get(0)
-                    .map(|w| w.value.clone())
-                    .unwrap_or_else(|| "unknown".to_string());
-                format!("🌤️ Weather in {}: {}°C, {}", city, temp, desc)
+        Ok(resp) => {
+            if resp.status() != StatusCode::NOT_FOUND {
+                match resp.json::<WeatherData>().await {
+                    Ok(data) => {
+                        let temp = &data.current_condition[0].temp_c;
+                        let desc = data.current_condition[0]
+                            .weather_desc
+                            .first()
+                            .map(|w| w.value.clone())
+                            .unwrap_or_else(|| "unknown".to_string());
+                        format!("🌤️ Weather in {}: {}°C, {}", city, temp, desc)
+                    }
+                    Err(_) => "❌ Couldn't parse weather data.".to_string(),
+                }
+            } else {
+                info!("Invalid city name: {}", city);
+                "⚠️ Sorry, We couldn't find that city.".to_string()
             }
-            Err(_) => "❌ Couldn't parse weather data.".to_string(),
-        },
+        }
         Err(_) => "⚠️ Failed to fetch weather. Try again later.".to_string(),
     };
 
